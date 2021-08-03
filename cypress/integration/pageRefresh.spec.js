d644c151cc2c780ab0c5f8dacd737ed2fff94948
srbro <srba.miljkovic@live.com>
2021-08-03 19:02:20 +0200

    blabla

--- cypress/integration/pageRefresh.spec.js
+++ cypress/integration/pageRefresh.spec.js
@@ -12,6 +12,7 @@ describe('Refreshing the page, recovers the current state', () => {
         cy.newTicket(selectors.column2.title, 'This is first ticket in the Column "In Progress"', selectors.column2.color)
         cy.newTicket(selectors.column3.title, 'This is first ticket in the Column "Done"', selectors.column3.color)
         cy.log('Reload and check if created tickets are still visible')
+        //cy.wait(1000)
         cy.reload()
         cy.ticketsExistInColumn(selectors.column1.title, ['This is first ticket in the Column "To Do"'], 1, selectors.column1.color)
         cy.ticketsExistInColumn(selectors.column2.title, ['This is first ticket in the Column "In Progress"'], 1, selectors.column2.color)
